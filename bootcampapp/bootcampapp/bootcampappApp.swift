3e8879e8dd3a22a82f0ceb761b0f58f48e2fd768
Brent <brent@tactec.ca>
2023-09-03 09:35:30 -0700

    push final wk1 homework app version for review

---
+++ bootcampapp/bootcampapp/bootcampappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcampappApp.swift
+//  bootcampapp
+//
+//  Created by Brent Reed on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct bootcampappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ playapp-test/playapp-test/playapp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  playapp_testApp.swift
+//  playapp-test
+//
+//  Created by Brent Reed on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct playapp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
